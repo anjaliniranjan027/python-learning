c5dc05aeae3ef93743976039a6660977dbf0f150
anjali niranjan <anjaliniranjan.official@gmail.com>
2026-01-02 16:24:39 +0530

    Add Day 01 Python basics

---
+++ Day - 01/variables.py
@@ -0,0 +1,24 @@
+# Variable = A container for stoting value (string, integer, float, boolen)
+
+# Strings
+first_name = "Python"
+print(first_name)
+# printing using formatted string
+print(f"Hello {first_name}")
+
+# Integer
+age = 24
+quantity = 3
+
+print(f"You are {age} years old")
+print(f"you are buying {quantity} items")
+
+# Float
+price = 10.99
+gpa = 8.7
+distance = 5.5
+print(f"Your gpa is {gpa}")
+
+# Boolean
+is_student = True
+print(f"Are you a student?: {is_student}")
